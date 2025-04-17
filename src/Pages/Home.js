f1354452cdb7db8abcf4152798feeedc883594e1
Farouq <farouqabubakar99@gmail.com>
2025-04-17 21:02:15 +0100

    Portfolio code added

---
+++ src/Pages/Home.js
@@ -0,0 +1,22 @@
+import HeroSection from "../Components/HeroSection";
+import Patnerships from "../Components/Patnerships";
+import Lorem from "../Components/Lorem";
+import Mansits from "../Components/Man";
+import Team from "../Components/Team";
+import Footer from "../Components/Footer";
+
+const Homepage = () => {
+    return ( 
+        <div>
+            <HeroSection />
+            <Patnerships />
+            <Lorem />
+            <Mansits />
+            <Team />
+            <Footer />
+        </div>
+
+     );
+}
+ 
+export default Homepage;
---
+++ src/Pages/Portfolio.js
@@ -0,0 +1,49 @@
+import NavigationBar from "../Components/Navigation";
+import landingpage from "../Images/Landin page.svg";
+import webpage from "../Images/Web page.svg";
+import rocket from "../Images/Rocket.svg";
+import shopping from "../Images/Shopping.svg";
+import geometric from "../Images/Geometric.svg";
+import trick from "../Images/Trick or treat.svg";
+import halloween from "../Images/Halloween.svg";
+import virtualx from "../Images/Virtual.png";
+import Footer from "../Components/Footer";
+import "./Portfolio.css";
+
+
+const Portfolio = () => {
+    return (
+        <> 
+        <section className="portfolio">
+            <div className="portfolio-texts">
+               <h4>W O R K S</h4>
+               <h3>Portfolio</h3>
+               <p>
+                Lorem ipsum, dolor sit amet <br />consectetur
+                <br />adipisicing elit.
+                </p> 
+            </div>
+
+            <div className="portfolio-img">
+                <img src={landingpage} alt="Landing Page" />
+                <img src={webpage} alt="Web design page" />
+                <img src={rocket} alt="Rocket your biz" />
+                <img src={shopping} alt="Shopping" />
+                <img src={geometric} alt="Geometric shapes" />
+                <img src={trick} alt="Trick or Treat" />
+                <img src={halloween} alt="Halloween" />
+                <img src={virtualx} alt="Virtual Experience" />
+            </div>
+
+            <div className="learn-button-div">
+                <button className='learn-button'>Learn More</button>    
+            </div>
+
+            <Footer />
+        </section>
+
+        </>
+     );
+}
+ 
+export default Portfolio;
