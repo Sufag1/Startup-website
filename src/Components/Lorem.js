27542612eeb4c545465c38faa0fb079b6bf3dc5e
Farouq <farouqabubakar99@gmail.com>
2025-04-17 21:07:15 +0100

    Lorem Section code added

---
+++ src/Components/Lorem.js
@@ -0,0 +1,100 @@
+.partneships {
+    background-color: #FFFFFF;
+    padding: 20px;
+
+}
+
+.partnership-texts {
+    text-align: center;
+    margin-bottom: 40px;
+}
+
+.partnership-logos {
+    max-width: 200px;
+    margin: 0 auto;
+    
+
+}
+
+.partnership-icons {
+    margin-bottom: 40px;
+}
+
+.learn-button {
+    width: 170px;
+    height: 48px;
+    border-radius: 5px;
+    margin-left: 20px;
+    font-size: 20px;
+    margin-top: 25px;
+    border: none;
+    background-color: black;
+    margin-bottom: 40px;
+    color: #FFFFFF;
+
+}
+
+.learn-button-div {
+    max-width: 230px;
+    margin: 0 auto;
+}
+
+.partnership-texts h5{
+    font-family: 'Work Sans', sans-serif;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 125%;
+    letter-spacing: 5px;
+    text-align: center;
+    text-transform: uppercase;
+    color: #74c69d;
+
+}
+
+.partnership-texts p {
+    font-family: 'Work Sans', sans-serif;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 125%;
+    letter-spacing: 0px;
+    text-align: center;
+}
+
+
+@media (min-width: 1024px) {
+    .partnership-logos {
+        display: flex;
+        justify-content: center;
+        max-width: 1160px;
+        
+    }
+    .partnership-icons {
+        margin: 0 auto;
+        
+    }
+
+    .partnership-texts h3 {
+        font-family: Crimson Text;
+        font-weight: 700;
+        font-size: 40px;
+        line-height: 125%;
+        letter-spacing: 0px;
+        text-align: center;
+
+    }
+
+    .partnership-texts p {
+        font-family: 'Work Sans', sans-serif;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 125%;
+        letter-spacing: 0px;
+        text-align: center;
+
+    }
+
+
+    .learn-button-div {
+        margin-top: 55px;
+    }
+}
